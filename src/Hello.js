2788626fbd837a2861c990f18810a55f290b51f3
Jun-cloud <kjy8827@gmail.com>
2020-09-13 18:42:52 +0900

    Create components

---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// component 생성
+function Hello() {
+    return (
+        <h1>Hello React!!</h1>
+    );
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Hello from './Hello';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// index.html의 id값이 root인 곳에 Hello를 rendering
+ReactDOM.render(<Hello />, document.getElementById("root"));
