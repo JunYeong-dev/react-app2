50954ec0d4ff6e34cbce04a08092d712a3e9cfcd
Jun-cloud <kjy8827@gmail.com>
2020-09-14 20:55:57 +0900

    Rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+// Hello를 불러옴
+import Hello from './Hello';
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      {/* Hello를 넣어줌 */}
+      <Hello />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './Hello';
+import App from './App';
 
-// index.html의 id값이 root인 곳에 Hello를 rendering
-ReactDOM.render(<Hello />, document.getElementById("root"));
+// index.html의 id값이 root인 곳에 App을 rendering(하나의 component만 rendering할 수 있음)
+// 2개 이상의 component를 rendering하고 싶을 경우 main component안에 다른 component를 불러오면 됨
+// 만약 App과 Hello를 root에 rendering하고 싶을 경우 App을 먼저 rendering한 후 App에서 Hello를 불러서 넣어줌 - App.js참고
+ReactDOM.render(<App />, document.getElementById("root"));
