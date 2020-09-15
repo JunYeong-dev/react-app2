e1491f108e9ae00c78cfb03401ae29a3c441f400
Jun-cloud <kjy8827@gmail.com>
2020-09-15 16:37:11 +0900

    JSX and Property

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 // Hello를 불러옴
 import Hello from './Hello';
 
+// JSX : JavaScript + HTML
+
+/* 
+html에서 보낸 값을 component에서 받음
+Movie(props)와 같이 보낸 모든 값을 받는 방법도 있음 - 이 경우 props는 object
+아래와 같은 방법은 title만 뽑아서 받는 방법
+*/
+function Movie({ title }){
+  return <h1>{ title }</h1>;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      {/* Hello를 넣어줌 */}
-      <Hello />
+      <Hello /> {/* Hello를 넣어줌 */}
+      {/* Movie라는 component에 title이라는 이름의 prop(property)을 Iron Man이라는 value로 넘겨준 것 */}
+      <Movie title="Iron Man"/>
     </div>
   );
 }
