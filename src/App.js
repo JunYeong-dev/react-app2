c80edb223240013a24d75bc388a2df943610e702
Jun-cloud <kjy8827@gmail.com>
2020-09-17 20:06:08 +0900

    Rendering for function

--- src/App.js
+++ src/App.js
@@ -13,25 +13,32 @@ function Movie({ title, image }){
   return (
     <div>
       <h1>{ title }</h1>
-      <img src={ image } />
+      <img src={ image } alt={ title }/>
     </div>
   );
 }
 
 const MoviesObj = [
   {
+    id: 1,
     title: "Iron Man",
     image: "http://images1.fanpop.com/images/image_uploads/new-wallpaper-iron-man-948765_1680_1050.jpg"
   },
   {
+    id: 2,
     title: "Doctor Strange",
     image: "https://mondrian.mashable.com/uploads%252Fcard%252Fimage%252F256764%252F597ddb0335d4402d8d440e7e3bb960d1.jpg%252F950x534__filters%253Aquality%252880%2529.jpg?signature=q0i1-OQQJ6aOkf7kOD2Aa4DBbig=&source=https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com"
   },
   {
+    id: 3,
     title: "Thor",
     image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdhgywazgeek0d.cloudfront.net%2Fwatcha%2Fimage%2Fupload%2Fc_fill%2Ch_1080%2Cq_80%2Cw_1920%2Fv1463389977%2Fme47yc-4770.jpg&f=1&nofb=1"
   }
 ]
+
+function renderMovie(MoviesObj){
+  return <Movie key={ MoviesObj.id } title={ MoviesObj.title } image={ MoviesObj.image } />
+}
 
 function App() {
   return (
@@ -45,8 +52,12 @@ function App() {
         MoviesObj.map(function(movie){  }) <-이 형식과 같은 예전 JavaScript 형식을 써도 됨
       */}
       { MoviesObj.map(movie => 
-        <Movie title={ movie.title } image={ movie.image }/>
+        <Movie key={ movie.id } title={ movie.title } image={ movie.image }/>
       ) }
+      {/*
+        아래 처럼 함수를 만들어 사용할 수도 있음 
+        { MoviesObj.map(renderMovie) } 
+      */}
     </div>
   );
 }
