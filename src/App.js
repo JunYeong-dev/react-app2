1cc269789f9468a8048e258bcc6c3d19644a873d
Jun-cloud <kjy8827@gmail.com>
2020-09-20 20:12:34 +0900

    State

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import PropTypes from "prop-types";
 
 // react는 자동적으로 class component의 render method를 실행
 class App extends React.Component{
+  // state는 object로서 component의 데이터를 넣는 공간
+  // 바꿀 데이터를 state안에 넣음
+  state = {
+    count: 0
+  }
+
   render() {
-    return <h1>Class component</h1>
+  return <h1>The number is: { this.state.count }</h1>
   }
 }
 
