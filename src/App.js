ac550a5125d652f3c391b385dba41a182a455d4e
Jun-cloud <kjy8827@gmail.com>
2020-09-21 19:32:10 +0900

    Function

--- src/App.js
+++ src/App.js
@@ -7,11 +7,26 @@ class App extends React.Component{
   // 바꿀 데이터를 state안에 넣음
   state = {
     count: 0
-  }
-
+  };
+  // JavaScript 코드 함수
+  add = () => {
+    console.log('add');
+  };
+  minus = () => {
+    console.log('minus');
+  };
   render() {
-  return <h1>The number is: { this.state.count }</h1>
-  }
+  return (
+  <div>
+    <h1>The number is: { this.state.count }</h1>
+    {/* 
+      this.add() : 이렇게 쓰게 되면 버튼을 클릭할 때 함수가 호출되는 것이 아닌,
+      화면이 로드 되면서 함수가 호출됨
+     */}
+    <button onClick={this.add}>Add</button>
+    <button onClick={this.minus}>Minus</button>
+  </div>
+  )};
 }
 
 export default App;
