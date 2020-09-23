34dd1bfbff0ca1198724f299ac085e6624832bcb
Jun-cloud <kjy8827@gmail.com>
2020-09-23 21:31:51 +0900

    Component lift cycle - Mounting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
+/*
+  component의 life cycle
+  Mounting - 가장 처음 로드 될 때
+  1. constructor()
+  2. static getDerivedStateFromProps() - 거의 사용하지 않음
+  3. render()
+  4. componentDidMount()
+  이 순서로 Mounting됨
+*/
+
 // react는 자동적으로 class component의 render method를 실행
 class App extends React.Component{
   // state는 object로서 component의 데이터를 넣는 공간
@@ -19,17 +29,17 @@ class App extends React.Component{
     this.setState({count: this.state.count - 1});
   };
   render() {
-  return (
-  <div>
-    <h1>The number is: { this.state.count }</h1>
-    {/* 
-      this.add() : 이렇게 쓰게 되면 버튼을 클릭할 때 함수가 호출되는 것이 아닌,
-      화면이 로드 되면서 함수가 호출됨
-     */}
-    <button onClick={this.add}>Add</button>
-    <button onClick={this.minus}>Minus</button>
-  </div>
-  )};
+    return (
+    <div>
+      <h1>The number is: { this.state.count }</h1>
+      {/* 
+        this.add() : 이렇게 쓰게 되면 버튼을 클릭할 때 함수가 호출되는 것이 아닌,
+        화면이 로드 되면서 함수가 호출됨
+      */}
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
+    )};
 }
 
 export default App;
