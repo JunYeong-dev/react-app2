eb6d6972bcdbc6ba27cb6d2664e135dbd48571cc
Jun-cloud <kjy8827@gmail.com>
2020-09-24 21:14:52 +0900

    Component lift cycle - Updating

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import PropTypes from "prop-types";
 
 /*
   component의 life cycle
+  
   Mounting - 가장 처음 로드 될 때
   1. constructor()
   2. static getDerivedStateFromProps() - 거의 사용하지 않음
   3. render()
   4. componentDidMount()
   이 순서로 Mounting됨
+
+  Updating - 무엇인가 변경이 되었을 때, 여기서는 add, minus함수를 사용했을 때를 예로 들 수 있음
+  1. static getDerivedStateFromProps()
+  2. shouldComponentUpdate()
+  3. render()
+  4. getSnapshotBeforeUpdate()
+  5. componentDidUpdate()
+
 */
 
 // react는 자동적으로 class component의 render method를 실행
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log('constructor');
+  }
+
   // state는 object로서 component의 데이터를 넣는 공간
   // 바꿀 데이터를 state안에 넣음
   state = {
@@ -28,7 +42,14 @@ class App extends React.Component{
   minus = () => {
     this.setState({count: this.state.count - 1});
   };
+  componentDidMount(){
+    console.log('componentDidMount');
+  }
+  componentDidUpdate(){
+    console.log('componentDidUpdate');
+  }
   render() {
+    console.log('render');
     return (
     <div>
       <h1>The number is: { this.state.count }</h1>
